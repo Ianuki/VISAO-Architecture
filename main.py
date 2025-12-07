6f4e461ab870a2ac224f77b6aad0c08ff24e7522
Ianukito <81605985+Ianuki@users.noreply.github.com>
2025-12-07 02:49:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 TOKENS_TABLE = {
-    "nop": {"bin": "0000", "arg_type":"reg"},
-    "and": {"bin": "0001", "arg_type":"reg"},
-    "or": {"bin": "0010", "arg_type":"reg"},
-    "nand": {"bin": "0011", "arg_type":"reg"},
-    "nor": {"bin": "0100", "arg_type":"reg"},
-    "xor": {"bin": "0101", "arg_type":"reg"},
-    "add": {"bin": "0110", "arg_type":"reg"},
-    "ls": {"bin": "0111", "arg_type":"reg"},
-    "sta": {"bin": "1000", "arg_type":"val"},
-    "lda": {"bin": "1001", "arg_type":"reg"},
-    "stb": {"bin": "1010", "arg_type":"val"},
-    "ldb": {"bin": "1011", "arg_type":"reg"},
+    "nop": {"bin": "0000", "arg_type":"null"},
+    "hlt": {"bin": "0001", "arg_type":"null"},
+    "sta": {"bin": "0010", "arg_type":"val"},
+    "lda": {"bin": "0011", "arg_type":"reg"},
+    "stb": {"bin": "0100", "arg_type":"val"},
+    "ldb": {"bin": "0101", "arg_type":"reg"},
+    "and": {"bin": "0110", "arg_type":"reg"},
+    "orr": {"bin": "0111", "arg_type":"reg"},
+    "nor": {"bin": "1000", "arg_type":"reg"},
+    "rsh": {"bin": "1001", "arg_type":"reg"},
+    "add": {"bin": "1010", "arg_type":"reg"},
+    "sub": {"bin": "1011", "arg_type":"reg"},
+    "jnz": {"bin": "1100", "arg_type":"val"},
+    "out": {"bin": "1101", "arg_type":"reg"},
+    "stm": {"bin": "1110", "arg_type":"val"},
+    "ldm": {"bin": "1111", "arg_type":"reg"}
 }
 
 REGISTERS_TABLE = {
@@ -23,35 +27,60 @@ REGISTERS_TABLE = {
 
 def assemble(source, filename):
     lines = source.split("\n")
+    line_count = 0
     output = ""
+    labels = {}
 
     for line in lines:
         if len(line) > 0:
-            if line[0] == "#":
+            if line[0] == ";":
                 continue
 
+        line = line.lstrip()
         words = line.split(" ")
 
-        if len(words) >= 2:
-            instruction = words[0]
-            param = words[1]
+        if len(words) >= 1 and words[0] != "":
+            instruction = words[0]      
+
+            if instruction.startswith("."):
+                name = line[1:]
+                name = name.rstrip()
+                labels[name] = line_count 
+                continue
 
             if not instruction in TOKENS_TABLE:
                 print("\nUnknown instruction: ", instruction)
                 return 
 
             output += TOKENS_TABLE[instruction]["bin"] + " "
+            line_count += 1
+
+            if TOKENS_TABLE[instruction]["arg_type"] == "null":
+
+                output += "0000 "
+                continue
 
             if TOKENS_TABLE[instruction]["arg_type"] == "reg":
+                param = words[1]
+
                 if not param in REGISTERS_TABLE:
                     print("\nUknown register: ", param)
                     return
                 
                 output += REGISTERS_TABLE[param] + " "
             elif TOKENS_TABLE[instruction]["arg_type"] == "val":
-                if not param.isdigit():
-                    print("\nValue parameter is not a number: ", param)
-                    return
+                param = words[1]
+                
+                if instruction == "jnz" and not param.isdigit():
+                    if not param in labels:
+                        print(f"Label {param} does not exist")
+                        return
+
+                    param = labels[param]
+                else:
+                    if not param.isdigit():
+                        print("\nValue parameter is not a number: ", param)
+                        return
                 
                 output += format(int(param), '04b') + " "
     
